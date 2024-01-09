cde99542c7f829fe7b2630837481e07a0af804f6
Kirill <kirill.l.2004@yandex.ru>
2024-01-09 17:54:48 +0300

    small changes

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -29,13 +29,13 @@ const Registration: FC = () => {
           <h2>Регистрация</h2>
 
           <Form.Group controlId="login" className='d-flex flex-column align-items-center mt-2'>
-            <Form.Label>Логин<Form.Control
+            <Form.Label>Логин</Form.Label>
+            <Form.Control
               type="text"
               placeholder="Введите ваш логин"
               value={login}
               onChange={(e: ChangeEvent<HTMLInputElement>) => setLogin(e.target.value)}
             />
-            </Form.Label>
           </Form.Group>
 
           <Form.Group controlId="password" className='d-flex flex-column align-items-center mt-1'>
